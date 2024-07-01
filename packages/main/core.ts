414b6f3406cdd0b80c446b93670f76a16f6e5503
Vincent-the-gamer <erxianqiaottjt@163.com>
2024-07-01 17:04:03 +0800

    feat: update SDK && change the way of setting proxies.

--- packages/main/core.ts
+++ packages/main/core.ts
@@ -1,5 +1,13 @@
 // @ts-ignore
-import { GoogleGenerativeAI } from "../generative-ai/index"
+import { Content, GoogleGenerativeAI } from "@google/generative-ai"
+import { setGlobalDispatcher, ProxyAgent } from "undici"
+import { proxyHost, proxyPort } from "../../proxy"
+
+// proxy settings
+const dispatcher = new ProxyAgent({ 
+  uri: new URL(`http://${proxyHost}:${proxyPort}`).toString() 
+});
+setGlobalDispatcher(dispatcher); 
 
 // please create file `.env` in the root of workspace, add write
 // API_KEY = xxx
@@ -7,17 +15,17 @@ import dotenv from "dotenv"
 dotenv.config()
 
 // Read your api key from dotenv file: process.env.API_KEY
-const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+const genAI = new GoogleGenerativeAI(process.env.API_KEY!);
 
-export let history: Record<string, any>[] = []
+export let history: Content[] = []
 
 // 纯文本对话
 export async function textOnlyChat(message: string) {
 
-    // For text-only input, use the gemini-pro model
-    // 纯文本聊天请使用gemini-pro模型
+    // For text-only input, use the gemini-1.5-flash model
+    // 纯文本聊天请使用gemini-1.5-flash模型
 
-    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
   
     const chat = model.startChat({
       history
